61668eabcf1b15517e497cc742eec1f2e500ee6e
keremkaratass <karatas.kerem2000@gmail.com>
2023-03-26 10:59:01 +0300

    from in-memoryrepository to jpa repository

--- src/main/java/kodlama/io/rentacar/business/abstracts/BrandService.java
+++ src/main/java/kodlama/io/rentacar/business/abstracts/BrandService.java
@@ -1,6 +1,6 @@
 package kodlama.io.rentacar.business.abstracts;
 
-import kodlama.io.rentacar.entities.concretes.Brand;
+import kodlama.io.rentacar.entities.Brand;
 
 import java.util.List;
 
@@ -8,6 +8,7 @@ public interface BrandService {
     List<Brand> getAll();
     Brand getById(int id);
     Brand add(Brand brand);
-    Brand update(int id,Brand brand); //aslında tek Brand brand yeterliydi ancak api standartları için bu önemli!!
+    Brand update(int id, Brand brand);
     void delete(int id);
+
 }
--- src/main/java/kodlama/io/rentacar/business/concretes/BrandManager.java
+++ src/main/java/kodlama/io/rentacar/business/concretes/BrandManager.java
@@ -1,48 +1,49 @@
 package kodlama.io.rentacar.business.concretes;
 
 import kodlama.io.rentacar.business.abstracts.BrandService;
-import kodlama.io.rentacar.entities.concretes.Brand;
-import kodlama.io.rentacar.repository.abstracts.BrandRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import kodlama.io.rentacar.entities.Brand;
+import kodlama.io.rentacar.repository.BrandRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-@Service
-public class BrandManager implements BrandService {
-  private final  BrandRepository repository;
 
-    @Autowired
-    public BrandManager(BrandRepository repository) {
-        this.repository = repository;
-    }
+@Service
+@AllArgsConstructor
+public class BrandManager implements BrandService {
+    private final BrandRepository repository;
 
     @Override
     public List<Brand> getAll() {
-        return repository.getAll();
+        return repository.findAll();
     }
 
     @Override
     public Brand getById(int id) {
-        return repository.getById(id);
+        checkIfBrandExists(id);
+        return repository.findById(id).orElseThrow();
     }
 
     @Override
     public Brand add(Brand brand) {
-        return repository.add(brand);
+        return repository.save(brand);
     }
 
     @Override
     public Brand update(int id, Brand brand) {
-        return repository.update(id, brand);
+        checkIfBrandExists(id);
+        brand.setId(id);
+        return repository.save(brand);
     }
 
     @Override
     public void delete(int id) {
-        repository.delete(id);
+        checkIfBrandExists(id);
+        repository.deleteById(id);
     }
 
-    // ! Business Rules
-
-
-
+    // !Business Rules
+    private void checkIfBrandExists(int id) {
+        if (!repository.existsById(id)) throw new RuntimeException("Böyle bir marka mevcut değil!");
+    }
 }
